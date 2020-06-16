c8e8445b4cb5b2965c8ad411dfa3580b361b4b06
fanchaojie0408 <1452663737@qq.com>
2020-06-16 20:03:40 +0800

    第一次提交

---
+++ src/main/java/com/qfedu/Dome.java
@@ -0,0 +1,13 @@
+package com.qfedu;
+
+/**
+ * @Description: TODO
+ * @Author FCJ
+ * @Date 2020/6/16 19:59
+ * @Version V1.0
+ */
+public class Dome {
+    public static void main(String[] args) {
+        System.out.println("w");
+    }
+}
