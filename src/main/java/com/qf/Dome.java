c9e050fc0452ee2b73f5ab4192080fe6cefa0f30
fanchaojie0408 <1452663737@qq.com>
2020-06-16 17:06:03 +0800

    Initial commit

---
+++ src/main/java/com/qf/Dome.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+/**
+ * @Description: TODO
+ * @Author FCJ
+ * @Date 2020/6/16 16:47
+ * @Version V1.0
+ */
+public class Dome {
+    private String name;
+    private String id;
+    private String password;
+}
